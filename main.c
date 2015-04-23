f3383c6b9a7d25d23c766e76fa86257f03ceab36
Makoto Yano <makoto.yano@soliton.co.jp>
2015-04-23 18:25:51 +0900

    PIN D0 D1 で振動させてブザーを鳴らす

--- main.c
+++ main.c
@@ -5,12 +5,24 @@
  */
 
 #include <avr/io.h>
+#include <util/delay.h>
+
+#define TRUE 1
+#define FALSE 0
+#define NULL '\0'
+
+// delay = 60
 
 int main(void)
 {
-    /* insert your hardware initialization here */
-    for(;;){
-        /* insert your main loop code here */
-    }
-    return 0;   /* never reached */
+  DDRD = 0b00000011;
+  unsigned char pin_c = 0;
+  for(;;){
+    pin_c = PINC & 0b00111111;
+    PORTD = 0b00000010;
+    _delay_ms(0.3);
+    PORTD = 0b00000001;
+    _delay_ms(0.3);
+  }
+  return 0;
 }
